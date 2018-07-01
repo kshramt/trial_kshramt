d68f8ba549e8da75f3e156b0887e20df96a7b0a9
kshramt <kshramt@example.com>
2018-07-01 22:26:16 +0900

    Stabilize (9.8 s, 5.0 s)

--- Prefix_Beam_Search/prefix_beam_search.py
+++ Prefix_Beam_Search/prefix_beam_search.py
@@ -81,14 +81,14 @@ def _logsumexp2(x: cython.double, y: cython.double) -> cython.double:
     m = max(x, y)
     if m <= NINF:
         return NINF
-    return math.log(math.exp(x - m) + math.exp(y - m)) + m
+    return math.log1p(math.exp(x - m) + math.exp(y - m)) + m
 
 
 def _logsumexp3(x: cython.double, y: cython.double, z: cython.double) -> cython.double:
     m = max(x, y, z)
     if m <= NINF:
         return NINF
-    return math.log(math.exp(x - m) + math.exp(y - m) + math.exp(z - m)) + m
+    return math.log1p(math.exp(x - m) + math.exp(y - m) + math.exp(z - m)) + m
 
 
 def main():
