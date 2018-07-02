46288861820efa289c80f2abcc6986be98845fae
kshramt <kshramt@example.com>
2018-07-02 09:38:30 +0900

    Cache _logsumexp results

--- Prefix_Beam_Search/prefix_beam_search.py
+++ Prefix_Beam_Search/prefix_beam_search.py
@@ -21,7 +21,7 @@ class PrefixBeamSearch(object):
     def search(self, width: int):
         t: int = -1
         path_new = EMPTY
-        candidates_prev = [(_logsumexp2(*logpb_logpn(t, path_new, self.logpred, self.cache)), path_new)]
+        candidates_prev = [(logpb_logpn_logp(t, path_new, self.logpred, self.cache)[2], path_new)]
         class_range = range(len(self.logpred[0]))
         for t in range(len(self.logpred)):
             candidates_new = set()
@@ -31,41 +31,42 @@ class PrefixBeamSearch(object):
                     path_new = path_prev if c == BLANK else path_prev + (c,)
                     if path_new in candidates_new:
                         continue
-                    candidates_new.add((_logsumexp2(*logpb_logpn(t, path_new, self.logpred, self.cache)), path_new))
+                    candidates_new.add((logpb_logpn_logp(t, path_new, self.logpred, self.cache)[2], path_new))
             candidates_prev = sorted(candidates_new)[-width:]
         return [(path, ll) for ll, path in candidates_prev]
 
 
 def init_cache(T):
     cache = {t: dict() for t in range(T)}
-    cache[-1] = {EMPTY: (0, NINF)}
+    cache[-1] = {EMPTY: (0, NINF, 0)}
     return cache
 
 
-def logpb_logpn(t: int, path, logpred, cache):
+def logpb_logpn_logp(t: int, path, logpred, cache):
     cache_t = cache[t]
     if path in cache_t:
         return cache_t[path]
     if path is EMPTY:
-        logPb_t1_empty, logPn_t1_empty = cache[t - 1][EMPTY]
-        ret = (logpred[t][BLANK] + logPb_t1_empty, NINF)
+        logPb_t1_empty, logPn_t1_empty, _ = cache[t - 1][EMPTY]
+        logPb_t_empty = logpred[t][BLANK] + logPb_t1_empty
+        ret = (logPb_t_empty, NINF, logPb_t_empty)
         cache_t[path] = ret
         return ret
     if t < 0:
-        ret = (NINF, NINF)
+        ret = (NINF, NINF, NINF)
         cache_t[path] = ret
         return ret
 
-    logPb_t1_path, logPn_t1_path = logpb_logpn(t - 1, path, logpred, cache)
+    logPb_t1_path, logPn_t1_path, logP_t1_path = logpb_logpn_logp(t - 1, path, logpred, cache)
     logpred_t = logpred[t]  # optimized
-    logPb_t_path = logpred_t[BLANK] + _logsumexp2(logPb_t1_path, logPn_t1_path)
+    logPb_t_path = logpred_t[BLANK] + logP_t1_path
     c_last = path[-1]
-    logPb_t1_path1, logPn_t1_path1 = logpb_logpn(t - 1, path[:-1], logpred, cache)
+    logPb_t1_path1, logPn_t1_path1, logP_t1_path1 = logpb_logpn_logp(t - 1, path[:-1], logpred, cache)
     if (len(path) > 1) and path[-2] == c_last:
-        logPn_t_path = logpred_t[c_last] + _logsumexp2(logPn_t1_path, logPb_t1_path1,               )
+        logPn_t_path = logpred_t[c_last] + _logsumexp2(logPn_t1_path, logPb_t1_path1)
     else:
-        logPn_t_path = logpred_t[c_last] + _logsumexp3(logPn_t1_path, logPb_t1_path1, logPn_t1_path1)
-    ret = (logPb_t_path, logPn_t_path)
+        logPn_t_path = logpred_t[c_last] + _logsumexp2(logPn_t1_path, logP_t1_path1)
+    ret = (logPb_t_path, logPn_t_path, _logsumexp2(logPb_t_path, logPn_t_path))
     cache_t[path] = ret
     return ret
 
@@ -78,17 +79,12 @@ def logsoftmax(x):
 
 
 def _logsumexp2(x: cython.double, y: cython.double):
-    m = max(x, y)
-    if m == NINF:
+    if x > y:
+        return x + math.log1p(math.exp(y - x))
+    elif y == NINF:
         return NINF
-    return m + math.log(math.exp(x - m) + math.exp(y - m))
-
-
-def _logsumexp3(x: cython.double, y: cython.double, z: cython.double):
-    m = max(x, y, z)
-    if m == NINF:
-        return NINF
-    return m + math.log(math.exp(x - m) + math.exp(y - m) + math.exp(z - m))
+    else:
+        return y + math.log1p(math.exp(x - y))
 
 
 def main():
