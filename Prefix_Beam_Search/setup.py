ddf389044ca11e64c00be8ba50db4c552e8b9618
kshramt <kshramt@example.com>
2022-10-22 20:52:09 +0900

    Cleanups

--- Prefix_Beam_Search/setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-import numpy as np
-
-setup(
-    ext_modules=cythonize("prefix_beam_search.py"),
-    include_dirs=[np.get_include()],
-)
